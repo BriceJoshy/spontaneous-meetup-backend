0f98481f718dbfa418bb9845de9b1525130d37a5
BRICE JOSHY <bricejoshy99@gmail.com>
2025-02-14 22:36:21 +0530

    redis client for better performance

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,9 +1,11 @@
 const Redis = require("ioredis");
+
 const redis = new Redis({
   host: "127.0.0.1",
   port: 6379,
+  maxRetriesPerRequest: null, // ✅ Required for BullMQ
 });
-
+redis.on("connect", () => console.log("✅ Redis Connected!"));
 redis.on("error", (err) => {
   console.error("Redis Error:", err);
 });
